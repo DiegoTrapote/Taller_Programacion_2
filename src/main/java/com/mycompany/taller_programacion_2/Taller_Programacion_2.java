b94dd9944974ecf23fa2735b5965515ef5599787
Diego_Trapote <Diego_Trapote@Diego>
2025-10-09 14:14:02 -0300

    Creacion de proyecto

---
+++ src/main/java/com/mycompany/taller_programacion_2/Taller_Programacion_2.java
@@ -0,0 +1,8 @@
+package com.mycompany.taller_programacion_2;
+
+public class Taller_Programacion_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
