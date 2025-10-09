de64640830ebd5d86bdb11ac160b03334d25ea57
juanf <juanf@PCJUAN>
2025-10-09 14:47:37 -0300

    taller 16/06

--- src/main/java/com/mycompany/taller_programacion_2/GUI/Gestion_Autos.java
+++ src/main/java/com/mycompany/taller_programacion_2/GUI/Gestion_Autos.java
@@ -6,14 +6,14 @@ package com.mycompany.taller_programacion_2.GUI;
 
 /**
  *
- * @author Diego_Trapote
+ * @author juanf
  */
-public class NewJFrame extends javax.swing.JFrame {
+public class Gestion_Autos extends javax.swing.JFrame {
 
     /**
-     * Creates new form NewJFrame
+     * Creates new form Gestion_Autos
      */
-    public NewJFrame() {
+    public Gestion_Autos() {
         initComponents();
     }
 
@@ -32,11 +32,11 @@ public class NewJFrame extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 988, Short.MAX_VALUE)
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 835, Short.MAX_VALUE)
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
@@ -59,20 +59,20 @@ public class NewJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Gestion_Autos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Gestion_Autos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Gestion_Autos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Gestion_Autos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);
+                new Gestion_Autos().setVisible(true);
             }
         });
     }
