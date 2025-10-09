00fd6a45bf2ddcdf46bab670d20a842a9e1cdd75
Diego_Trapote <Diego_Trapote@Diego>
2025-10-09 14:24:53 -0300

    Creacion de paquetes

---
+++ src/main/java/com/mycompany/taller_programacion_2/Modelo/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller_programacion_2.Modelo;
+
+/**
+ *
+ * @author Diego_Trapote
+ */
+public class NewClass {
+    
+}
